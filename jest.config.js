08590e2352ae54d9e2013315b4d46b8484503e88
caiolul <caiolucena.il@gmail.com>
2020-10-04 10:16:25 -0300

    feat: Creating setup for testings

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   clearMocks: true,
-  preset: 'ts-jest',
   projects: ['<rootDir>/packages/**/jest.config.js'],
   testEnvironment: 'node',
   testMatch: ['*.spec.ts', '*.spec.tsx']
---
+++ packages/mobile/src/test/ntest.spec.tsx
@@ -0,0 +1,5 @@
+describe('Test mobile', () => {
+  it('should do something', () => {
+    expect(1 + 3).toEqual(4)
+  })
+})
